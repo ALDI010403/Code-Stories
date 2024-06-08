3ea18159000a331495f087be74b1c9332302c319
Aldi <aldi010403@gmail.com>
2024-06-08 10:11:30 +0700

    create database

---
+++ app/src/main/java/com/aldi/codestories/data/local/database/RemoteKeys.kt
@@ -0,0 +1,13 @@
+package com.aldi.codestories.data.local.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "remote_keys")
+data class RemoteKeys(
+
+    @PrimaryKey val id: String,
+    val prevKey: Int?,
+    val nextKey: Int?
+
+)
